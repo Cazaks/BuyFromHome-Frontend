7b4c64311e4dacbdb7e0ef742aa3b9e90fe0f57e
calebezak@gmail.com <calebezak@gmail.com>
2026-09-04 05:27:13 +0100

    Updating App.jsx to include Auth.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import AuthProvider from './context/AuthContext';
 import CartProvider from './context/CartContext';
 import MainLayout from './layout/MainLayout';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+import Auth from './pages/Auth';
 
 function App() {
   return (
     <ThemeProvider>
       <AuthProvider>
         <CartProvider>
-          <Routes>
-            <Route element={<MainLayout />}>
-              <Route path="/" element={<Home />} />
-              <Route path="/cart" element={<Cart />} />
-            </Route>
-          </Routes>
+          <BrowserRouter>
+            <Routes>
+              <Route element={<MainLayout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/auth" element={<Auth />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
         </CartProvider>
       </AuthProvider>
     </ThemeProvider>
