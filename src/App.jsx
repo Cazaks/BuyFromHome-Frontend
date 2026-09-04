9626be28526eade605529c91483b3fc1c0836f87
calebezak@gmail.com <calebezak@gmail.com>
2026-09-04 06:26:13 +0100

    updating App.jsx and MainLayout.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import MainLayout from './layout/MainLayout';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import Auth from './pages/Auth';
+import CreateProduct from './pages/CreateProduct';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/cart" element={<Cart />} />
                 <Route path="/auth" element={<Auth />} />
+                <Route path="/admin/products/new" element={<CreateProduct />} />
               </Route>
             </Routes>
           </BrowserRouter>
